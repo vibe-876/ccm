4827ca95191acf0afe459a72b31deff5f7140f1b
cam <cameronayres123@gmail.com>
2025-09-21 02:27:20 +0100

    Added "C-c C-l", which'll compile, but not run gdb yet.

--- ccm.el
+++ ccm.el
@@ -1,8 +1,13 @@
+(defun ccm-compile-and-run (makefile-directory)
+  "Test.disadsi"
+  (interactive "DMakefile Directory? ")
+  (let ((default-directory makefile-directory))
+    (compile "make")))
+
 (define-minor-mode ccm-mode
   "ccm-mode (Camilla's C Mode) is a minor mode
 for C programming. It is basically just calling
 `compile', and then `gdb'. This is meant to be
 used with `gdb-many-windows' set to non-nil."
   :lighter "ccm"
-  :keymap (list (cons (kbd "C-c C-l") (lambda () (interactive)
-					(message ":3")))))
+  :keymap (list (cons (kbd "C-c C-l") 'ccm-compile-and-run)))
